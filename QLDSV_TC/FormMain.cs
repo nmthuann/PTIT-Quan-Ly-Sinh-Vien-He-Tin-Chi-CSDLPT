55e57fb660376407fe6bafbdff7bad0d73a113f2
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2023-07-09 16:41:50 +0700

    Add project files.

---
+++ QLDSV_TC/FormMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLDSV_TC
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
